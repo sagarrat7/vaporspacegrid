89276b6debc5da77912e79525d5e2785f0814067
sagarrat7 <sagarrat7@gmail.com>
2022-09-30 18:57:38 -0700

    update test script to connect accounts

--- programs/vaporspacegrid/src/lib.rs
+++ programs/vaporspacegrid/src/lib.rs
@@ -10,15 +10,27 @@ pub mod vaporspacegrid {
     base_account.total_gifs = 0;
     Ok(())
   }
+  
+  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
 pub struct StartStuffOff<'info> {
-    #[account(init, payer = user, space = 9000)]
-    pub base_account: Account<'info, BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
 }
 
 #[account]
