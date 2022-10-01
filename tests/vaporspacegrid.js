a6e0fd1fbe9a4ebbf2b230823ed66141777c4d71
sagarrat7 <sagarrat7@gmail.com>
2022-09-30 18:38:14 -0700

    retrieve account data

--- tests/vaporspacegrid.js
+++ tests/vaporspacegrid.js
@@ -1,13 +1,30 @@
 const anchor = require('@project-serum/anchor');
 
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Vaporspacegrid;
-  const tx = await program.rpc.startStuffOff();
+	
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
