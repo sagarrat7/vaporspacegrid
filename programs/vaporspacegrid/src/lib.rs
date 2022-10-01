c806baf0c291528d790bd5cb931e103a50be4bd3
sagarrat7 <sagarrat7@gmail.com>
2022-09-30 19:22:01 -0700

    update program id & deploy to devnet

--- programs/vaporspacegrid/src/lib.rs
+++ programs/vaporspacegrid/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6nKzKeV6BkjpvtB5uyEGgbWgxnFKqX1n38J8NLG4o2CT");
 
 #[program]
 pub mod vaporspacegrid {
